7c405e94b947ce3ee9d2f2172d3290cf26104470
Eduarda Olímpio <93235055+MEduardaOl@users.noreply.github.com>
2023-06-12 16:32:41 -0300

    Add jump and respawn

--- player.gd
+++ player.gd
@@ -2,8 +2,10 @@ extends KinematicBody
 
 const MOVE_SPEED = 5
 var velocity = Vector3.ZERO
-var mouse_sense = 0.1
+var mouse_sense = 0.05
+var jump_force = 18
 var gravity = 70
+var time_jumped = 0
 
 onready var pivot = $Pivot
 onready var camera = $Pivot/Camera
@@ -12,9 +14,14 @@ func _ready():
 	pass
 	
 func _physics_process(delta):
+	if (is_on_floor()):
+		time_jumped = 0
+		
+	respawn()
 	var input_vector = get_input_vector()
 	apply_movement(input_vector)
 	apply_gravity(delta)
+	jump()
 	velocity = move_and_slide(velocity, Vector3.UP)
 	
 func _input(event):
@@ -25,8 +32,8 @@ func _input(event):
 	
 func get_input_vector():
 	var input_vector = Vector3.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.z = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.x = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
+	input_vector.z = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
 	return input_vector.normalized()
 	
 func apply_movement(input_vector):
@@ -35,3 +42,18 @@ func apply_movement(input_vector):
 
 func apply_gravity(delta):
 	velocity.y -= gravity * delta
+
+func jump():
+	if(is_on_floor() and Input.is_action_just_pressed("ui_select")):
+		if(time_jumped == 0):
+			velocity.y = jump_force
+			time_jumped = 1
+	
+	if not (is_on_floor() and Input.is_action_just_pressed("ui_select")):
+		if(time_jumped == 1):
+			velocity.y = jump_force
+			time_jumped = 2	
+
+func respawn():
+	if(Input.is_action_just_pressed("ui_focus_next")):
+		get_tree().reload_current_scene()
